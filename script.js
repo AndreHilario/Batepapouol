6f15bf9aae79e3aa8eac766ab38f5097ad579934
AndreHilario <andre18hilario@gmail.com>
2023-01-19 14:00:41 -0300

    Versão com buscar e exibir mensagens do servidor

--- script.js
+++ script.js
@@ -30,5 +30,49 @@ function nomeDoUsuarioNaoChegou(error){
         return;
     }
 }
+function exibirMensagens(){ 
+    const chat = document.querySelector('.mensagens');
+    for(let i = 0; i < mens.length; i++){ 
+        if (mens[i].type === "status"){   
+            let template = `
+                <div class="status"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template; 
+        } else if (mens[i].type ==="message"){ 
+            let template2 = `
+                <div class="normais"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> para 
+                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template2;  
+        } else if (mens[i].type ==="private_message"){ 
+            let template3 = `
+                <div class="reservadas"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> reservadamente para 
+                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template3; 
+            } 
+    }
+} 
+
+function buscarMensagensNoServidor(){ 
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(mensagensChegaram); 
+    promise.catch(deuErroAoPegarMensagens); 
+
+}
+function mensagensChegaram(answer){ 
+    console.log('As mensagens chegaram com êxito!!'); 
+    console.log(answer); 
+    mens = answer.data; 
+    
+    exibirMensagens(); 
+}
+
+function deuErroAoPegarMensagens(error){ 
+    console.log('Deu erro ao pergar as mensagens no servidor'); 
+    console.log(error); 
+}
+buscarMensagensNoServidor();
+
 
 
