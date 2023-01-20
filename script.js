9728675d38387b7a4946a69b8e469c3ac03a717b
AndreHilario <andre18hilario@gmail.com>
2023-01-20 14:01:50 -0300

    Teste 4

--- script.js
+++ script.js
@@ -14,6 +14,52 @@ function enviarNomeDoUsuario(){
 }
 enviarNomeDoUsuario();
 
+function exibirMensagens(){ 
+    const chat = document.querySelector('.mensagens');
+    chat.innerHTML = '';
+    for(let i = 0; i < mens.length; i++){ 
+        if (mens[i].type === "status"){   
+            let template = `
+                <div data-test="message" class="status"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template; 
+        } else if (mens[i].type ==="message"){ 
+            let template2 = `
+                <div data-test="message" class="normais"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> para 
+                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template2;  
+        } else if (mens[i].type ==="private_message" && (mens[i].from === nomeDoUsuario || mens[i].to === nomeDoUsuario)){ 
+            let template3 = `
+                <div data-test="message" class="reservadas"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> reservadamente para 
+                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
+                
+                chat.innerHTML = chat.innerHTML + template3; 
+            } 
+    }
+} 
+
+function buscarMensagensNoServidor(){ 
+    
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(mensagensChegaram); 
+    promise.catch(deuErroAoPegarMensagens); 
+
+}
+function mensagensChegaram(answer){ 
+    console.log('As mensagens chegaram com êxito!!'); 
+    console.log(answer); 
+    mens = answer.data; 
+    
+    exibirMensagens(); 
+}
+
+function deuErroAoPegarMensagens(error){ 
+    console.log('Deu erro ao pergar as mensagens no servidor'); 
+    console.log(error); 
+}
+
+
 function nomeDoUsuarioChegou(answer){
     buscarMensagensNoServidor();
     console.log('Deu tudo certo, nome chegou!');
@@ -58,50 +104,6 @@ function erroUsuarioOffline(erro){
     clearInterval(parada);
 }
 
-function exibirMensagens(){ 
-    const chat = document.querySelector('.mensagens');
-    
-    for(let i = 0; i < mens.length; i++){ 
-        if (mens[i].type === "status"){   
-            let template = `
-                <div data-test="message" class="status"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> ${mens[i].text}</p></div>`; 
-                
-                chat.innerHTML = chat.innerHTML + template; 
-        } else if (mens[i].type ==="message"){ 
-            let template2 = `
-                <div data-test="message" class="normais"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> para 
-                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
-                
-                chat.innerHTML = chat.innerHTML + template2;  
-        } else if (mens[i].type ==="private_message" && (mens[i].from === nomeDoUsuario || mens[i].to === nomeDoUsuario)){ 
-            let template3 = `
-                <div data-test="message" class="reservadas"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> reservadamente para 
-                <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
-                
-                chat.innerHTML = chat.innerHTML + template3; 
-            } 
-    }
-} 
-
-function buscarMensagensNoServidor(){ 
-    
-    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promise.then(mensagensChegaram); 
-    promise.catch(deuErroAoPegarMensagens); 
-
-}
-function mensagensChegaram(answer){ 
-    console.log('As mensagens chegaram com êxito!!'); 
-    console.log(answer); 
-    mens = answer.data; 
-    
-    exibirMensagens(); 
-}
-
-function deuErroAoPegarMensagens(error){ 
-    console.log('Deu erro ao pergar as mensagens no servidor'); 
-    console.log(error); 
-}
 
 function enviarMensagens(){
 
@@ -118,7 +120,6 @@ function enviarMensagens(){
 
     promessa2.then(mensagemDigitadaChegou);
     promessa2.catch(deuRuimAoEnviar);
-    textoDaMensagem.value = '';
 }
 
 function mensagemDigitadaChegou(resposta){
