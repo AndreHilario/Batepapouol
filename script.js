634a3f148dfb9b98fd8d280fada292752057cc53
AndreHilario <andre18hilario@gmail.com>
2023-01-20 12:29:34 -0300

    Versão com função de enivar mensagens implementada

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function exibirMensagens(){
                 <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
                 
                 chat.innerHTML = chat.innerHTML + template2;  
-        } else if (mens[i].type ==="private_message"){ 
+        } else if (mens[i].type ==="private_message" && (mens[i].from === nomeDoUsuario || mens[i].to === nomeDoUsuario)){ 
             let template3 = `
                 <div class="reservadas"><p><span>(${mens[i].time})</span> <mark>${mens[i].from}</mark> reservadamente para 
                 <mark>${mens[i].to}</mark>: ${mens[i].text}</p></div>`; 
@@ -101,6 +101,40 @@ function deuErroAoPegarMensagens(error){
     console.log(error); 
 }
 
+function enviarMensagens(){
+
+    let textoDaMensagem = document.querySelector('.texto').value;
+
+    const mensagem = {
+        from: nomeDoUsuario,
+        to: "Todos",
+        text: textoDaMensagem,
+        type: "message"
+    }
+
+    const promessa2 = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages' , mensagem);
+
+    promessa2.then(mensagemDigitadaChegou);
+    promessa2.catch(deuRuimAoEnviar);
+    textoDaMensagem.value = '';
+}
+
+function mensagemDigitadaChegou(resposta){
+    console.log('Sua mensagem digitada chegou');
+    console.log(resposta);
+
+    buscarMensagensNoServidor();
+    exibirMensagens();
+
+
+}
+
+function deuRuimAoEnviar(erro){
+    console.log('Sua mensagem digitada não chegou');
+    console.log(erro);
+
+    window.location.reload();
+}
 
 
 
